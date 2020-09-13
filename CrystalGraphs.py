562754ba17ecc023d9029a0580e69a652b8ecb83
Clay246 <63927010+Clay246@users.noreply.github.com>
2020-09-13 10:10:27 -0400

    Update CrystalGraphs.py
    Significant improvements to aesthetics and added lattice vectors and draw_path feature

--- CrystalGraphs.py
+++ CrystalGraphs.py
@@ -1,64 +1,284 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import itertools
+# use all_basis instead of args in the islice part of the function
 
 size = 50
-alpha = .75
+marker = 'o'
+
+plt.rcParams['grid.color'] = '#252525'
 
 fig = plt.figure()
 
 ax = Axes3D(fig)
+
+ax.set_facecolor('black')
+ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 0.0))
+ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 0.0))
+ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 0.0))
+
+x=[]
+y=[]
+z=[]
+
+def cubic(n, *args, draw_path=False):
+    all_basis = args
+    
+    # Lattice vectors
+    ax.plot([0,1],[0,0],[0,0], c='red', label='$\\vec{a}$')
+    ax.plot([0,0],[0,1],[0,0], c='blue', label='$\\vec{b}$')
+    ax.plot([0,0],[0,0],[0,1], c='green', label='$\\vec{c}$')
+
+    # Cube corners
+    for i, j, k in itertools.product(range(n+1), range(n+1), range(n+1)):
+        x.append(all_basis[0][0]+k)
+        y.append(all_basis[0][1]+j)
+        z.append(all_basis[0][2]+i)
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+
+    # Additional basis points
+    for index in range(1, len(all_basis)):
+        for i, j, k in itertools.product(range(n), range(n), range(n)):
+            x.append(all_basis[index][0]+k)
+            y.append(all_basis[index][1]+j)
+            z.append(all_basis[index][2]+i)     
+        ax.scatter(x, y, z, c=all_basis[index][3], s=size)
+        x.clear()
+        y.clear()
+        z.clear()
+
+    # Show path along lattice vectors for basis points
+    if draw_path == True:
+        for i in range(len(all_basis)):
+            ax.plot([0,all_basis[i][0]], [0,0], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [0,all_basis[i][1]], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [all_basis[i][1],all_basis[i][1]], [0,all_basis[i][2]], c='grey', linestyle=':')
+
+
+def fcc(n, *args, draw_path=False):
+    all_basis = args
+    
+    # Lattice vectors
+    ax.plot([0,1],[0,0],[0,0], c='red', label='$\\vec{a}$')
+    ax.plot([0,0],[0,1],[0,0], c='blue', label='$\\vec{b}$')
+    ax.plot([0,0],[0,0],[0,1], c='green', label='$\\vec{c}$')
+    
+    # Cube corners
+    for i, j, k in itertools.product(range(n+1), range(n+1), range(n+1)):
+        x.append(all_basis[0][0]+k)
+        y.append(all_basis[0][1]+j)
+        z.append(all_basis[0][2]+i)
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+
+    # Face points
+    for i, j, k in itertools.product(range(n+1), range(n), range(n)):
+        x.extend([all_basis[0][0]+.5+k, all_basis[0][0]+.5+k, all_basis[0][0]+i])
+        y.extend([all_basis[0][1]+.5+j, all_basis[0][1]+i, all_basis[0][1]+.5+k])
+        z.extend([all_basis[0][2]+i, all_basis[0][2]+.5+j, all_basis[0][2]+.5+j])
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+    
+    # Additional basis points
+    for index in range(1, len(all_basis)):
+        for i, j, k in itertools.product(range(n), range(n), range(n)):
+            x.extend([all_basis[index][0]+k, all_basis[index][0]+.5+k, all_basis[index][0]+k, all_basis[index][0]+.5+k])
+            y.extend([all_basis[index][1]+j, all_basis[index][1]+.5+j, all_basis[index][1]+.5+j, all_basis[index][1]+j])
+            z.extend([all_basis[index][2]+i, all_basis[index][2]+i, all_basis[index][2]+.5+i, all_basis[index][2]+.5+i])
+        ax.scatter(x, y, z, c=all_basis[index][3], s=size)
+        x.clear()
+        y.clear()
+        z.clear()
+
+    # Show path along lattice vectors for basis points
+    if draw_path == True:
+        for i in range(len(all_basis)):
+            ax.plot([0,all_basis[i][0]], [0,0], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [0,all_basis[i][1]], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [all_basis[i][1],all_basis[i][1]], [0,all_basis[i][2]], c='grey', linestyle=':')
+
+
+def bcc(n, *args, draw_path=False):
+    all_basis = args
+    
+    # Lattice vectors
+    ax.plot([0,1],[0,0],[0,0], c='red', label='$\\vec{a}$')
+    ax.plot([0,0],[0,1],[0,0], c='blue', label='$\\vec{b}$')
+    ax.plot([0,0],[0,0],[0,1], c='green', label='$\\vec{c}$')
+    
+    # Cube corners
+    for i, j, k in itertools.product(range(n+1), range(n+1), range(n+1)):
+        x.append(all_basis[0][0]+k)
+        y.append(all_basis[0][1]+j)
+        z.append(all_basis[0][2]+i)
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+
+    # Center points
+    for i, j, k in itertools.product(range(n), range(n), range(n)):
+        x.extend([all_basis[0][0]+.5+k])
+        y.extend([all_basis[0][1]+.5+j])
+        z.extend([all_basis[0][2]+.5+i])
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+
+    # Additional basis points
+    for index in range(1, len(all_basis)):
+        for i, j, k in itertools.product(range(n), range(n), range(n)):
+            x.extend([all_basis[index][0]+k, all_basis[index][0]+.5+k])
+            y.extend([all_basis[index][1]+j, all_basis[index][1]+.5+j])
+            z.extend([all_basis[index][2]+i, all_basis[index][2]+.5+i])
+        ax.scatter(x, y, z, c=all_basis[index][3], s=size)
+        x.clear()
+        y.clear()
+        z.clear()
+
+    # Show path along lattice vectors for basis points
+    if draw_path == True:
+        for i in range(len(all_basis)):
+            ax.plot([0,all_basis[i][0]], [0,0], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [0,all_basis[i][1]], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [all_basis[i][1],all_basis[i][1]], [0,all_basis[i][2]], c='grey', linestyle=':')
+
+
+def tetragonal(n, height_ratio, *args, draw_path=False):
+    all_basis = args
+    
+    # Lattice vectors
+    ax.plot([0,1],[0,0],[0,0], c='red', label='$\\vec{a}$')
+    ax.plot([0,0],[0,1],[0,0], c='blue', label='$\\vec{b}$')
+    ax.plot([0,0],[0,0],[0,height_ratio], c='green', label='$\\vec{c}$')
+    
+    # Cell corners
+    for i, j, k in itertools.product(range(n+1), range(n+1), range(n+1)):
+        x.append(all_basis[0][0]+k)
+        y.append(all_basis[0][1]+j)
+        z.append(all_basis[0][2]+i*height_ratio)
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+
+    # Additional basis points
+    for index in range(1, len(all_basis)):
+        for i, j, k in itertools.product(range(n), range(n), range(n)):
+            x.append(all_basis[index][0]+k)
+            y.append(all_basis[index][1]+j)
+            z.append(all_basis[index][2]+i*height_ratio)     
+        ax.scatter(x, y, z, c=all_basis[index][3], s=size)
+        x.clear()
+        y.clear()
+        z.clear()
+
+    # Show path along lattice vectors for basis points
+    if draw_path == True:
+        for i in range(len(all_basis)):
+            ax.plot([0,all_basis[i][0]], [0,0], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [0,all_basis[i][1]], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [all_basis[i][1],all_basis[i][1]], [0,all_basis[i][2]], c='grey', linestyle=':')
+
+
+def tetragonal_bc(n, height_ratio, *args, draw_path=False):
+    all_basis = args
+    
+    # Lattice vectors
+    ax.plot([0,1],[0,0],[0,0], c='red', label='$\\vec{a}$')
+    ax.plot([0,0],[0,1],[0,0], c='blue', label='$\\vec{b}$')
+    ax.plot([0,0],[0,0],[0,2], c='green', label='$\\vec{c}$')
+    
+    # Cube corners
+    for i, j, k in itertools.product(range(n+1), range(n+1), range(n+1)):
+        x.append(all_basis[0][0]+k)
+        y.append(all_basis[0][1]+j)
+        z.append(all_basis[0][2]+i*height_ratio)
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+
+    # Center points
+    for i, j, k in itertools.product(range(n), range(n), range(n)):
+        x.extend([all_basis[0][0]+.5+k])
+        y.extend([all_basis[0][1]+.5+j])
+        z.extend([all_basis[0][2]+.5*height_ratio+i*height_ratio])
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+
+    # Additional basis points
+    for index in range(1, len(all_basis)):
+        for i, j, k in itertools.product(range(n), range(n), range(n)):
+            x.extend([all_basis[index][0]+k, all_basis[index][0]+.5+k])
+            y.extend([all_basis[index][1]+j, all_basis[index][1]+.5+j])
+            z.extend([all_basis[index][2]+i*height_ratio, all_basis[index][2]+.5*height_ratio+i*height_ratio])
+        ax.scatter(x, y, z, c=all_basis[index][3], s=size)
+        x.clear()
+        y.clear()
+        z.clear()
+
+    # Show path along lattice vectors for basis points
+    if draw_path == True:
+        for i in range(len(all_basis)):
+            ax.plot([0,all_basis[i][0]], [0,0], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [0,all_basis[i][1]], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]], [all_basis[i][1],all_basis[i][1]], [0,all_basis[i][2]], c='grey', linestyle=':')
+
+
+def hexagonal(n, height_ratio, *args, draw_path=False):
+    all_basis = args # chjange to all_basis or vice versa
+    
+    # Lattice vectors
+    ax.plot([0,1],[0,0],[0,0], c='red', label='$\\vec{a}$')
+    ax.plot([0,-np.cos(np.pi/3)],[0,np.sin(np.pi/3)],[0,0], c='blue', label='$\\vec{b}$')
+    ax.plot([0,0],[0,0],[0,height_ratio], c='green', label='$\\vec{c}$')
+    
+    # Corner points
+    for i, j, k in itertools.product(range(n+1), range(n+1), range(n+1)):
+        x.extend([all_basis[0][0]+k+j*np.cos(2*np.pi/3)+np.cos(2*np.pi/3)*all_basis[0][1]])
+        y.extend([all_basis[0][1]*np.sin(2*np.pi/3)+j*np.sin(2*np.pi/3)])
+        z.extend([all_basis[0][2]+i*height_ratio])
+    ax.scatter(x, y, z, c=all_basis[0][3], s=size)
+    x.clear()
+    y.clear()
+    z.clear()
+    
+    # Addition basis points
+    for index in range(1, len(all_basis)):
+        for i, j, k in itertools.product(range(n), range(n), range(n)):
+            x.extend([all_basis[index][0]+k+j*np.cos(2*np.pi/3)+np.cos(2*np.pi/3)*all_basis[index][1]])
+            y.extend([all_basis[index][1]*np.sin(2*np.pi/3)+j*np.sin(2*np.pi/3)])
+            z.extend([all_basis[index][2]+i*height_ratio])
+        ax.scatter(x, y, z, c=all_basis[index][3], s=size)
+        x.clear()
+        y.clear()
+        z.clear()
+
+    # Show path along lattice vectors for basis points
+    if draw_path == True:
+        for i in range(len(all_basis)):
+            ax.plot([0,all_basis[i][0]], [0,0], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0],all_basis[i][0]+all_basis[i][1]*np.cos(2*np.pi/3)], [0,all_basis[i][1]*np.sin(2*np.pi/3)], [0,0], c='grey', linestyle=':')
+            ax.plot([all_basis[i][0]+all_basis[i][1]*np.cos(2*np.pi/3),all_basis[i][0]+all_basis[i][1]*np.cos(2*np.pi/3)], [all_basis[i][1]*np.sin(2*np.pi/3),all_basis[i][1]*np.sin(2*np.pi/3)], [0,all_basis[i][2]], c='grey', linestyle=':')
+
     
 
-def cubic(n, *args):
-    for basis, i, j, k in itertools.product(args, range(n), range(n), range(n)):
-        ax.scatter(basis[0]+k,
-                   basis[1]+j,
-                   basis[2]+i,
-                   c=basis[3], alpha=alpha, s=size)
 
-
-def fcc(n, *args):
-    for basis, i, j, k in itertools.product(args, range(n), range(n), range(n)):
-        ax.scatter([basis[0]+k, basis[0]+.5+k, basis[0]+k, basis[0]+.5+k],
-                   [basis[1]+j, basis[1]+.5+j, basis[1]+.5+j, basis[1]+j],
-                   [basis[2]+i, basis[2]+i, basis[2]+.5+i, basis[2]+.5+i],
-                   c=basis[3], alpha=alpha, s=size)
-
-
-def bcc(n, *args):
-    for basis, i, j, k in itertools.product(args, range(n), range(n), range(n)):
-        ax.scatter([basis[0]+k, basis[0]+.5+k],
-                   [basis[1]+j, basis[1]+.5+j],
-                   [basis[2]+i, basis[2]+.5+i],
-                   c=basis[3], alpha=alpha, s=size)
-
-
-def tetragonal(n, height_ratio, *args):
-    for basis, i, j, k in itertools.product(args, range(n), range(n), range(n)):
-        ax.scatter(basis[0]+k,
-                   basis[1]+j,
-                   basis[2]+i*height_ratio,
-                   c=basis[3], alpha=alpha, s=size)
-
-def tetragonal_bc(n, height_ratio, *args):
-    for basis, i, j, k in itertools.product(args, range(n), range(n), range(n)):
-        ax.scatter([basis[0]+k, basis[0]+.5+k],
-                   [basis[1]+j, basis[1]+.5+j],
-                   [basis[2]+i*height_ratio, basis[2]+.5*height_ratio+i*height_ratio],
-                   c=basis[3], alpha=alpha, s=size)
-
-
-def hexagonal(n, height_ratio, *args):
-    for basis, i, j, k in itertools.product(args, range(n), range(n), range(n)):
-        ax.scatter(basis[0]+k+j*np.cos(2*np.pi/3)+np.cos(2*np.pi/3)*basis[1],
-                   basis[1]*np.sin(2*np.pi/3)+j*np.sin(2*np.pi/3),
-                   basis[2]+i*height_ratio,
-                   c=basis[3], alpha=alpha, s=size)
-
-
-# The function below is not my code; thank you stack overflow for this solution
+# Thank you stack exchange for this solution
+# matplotlib's ax.set_aspect('equal') does not currently work for 3D plots
 # https://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
 # User: karlo
 def set_axes_equal(ax):
@@ -92,5 +312,6 @@ def set_axes_equal(ax):
 
 def show():
     set_axes_equal(ax)
+    ax.legend()
     plt.show()
 

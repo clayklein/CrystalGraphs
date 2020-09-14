a25d4c30ab2a66c87970d6d95372b0d7d6c41800
Clay246 <63927010+Clay246@users.noreply.github.com>
2020-09-14 13:19:21 -0400

    Update CrystalGraphs.py

--- CrystalGraphs.py
+++ CrystalGraphs.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import itertools
-# use all_basis instead of args in the islice part of the function
 
 size = 50
 
